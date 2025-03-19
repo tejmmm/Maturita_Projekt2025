6d1b60242e05ff4cc41180d2df56603690ce4fd3
Ondřej Zbořil <101326894+tejmmm@users.noreply.github.com>
2025-03-19 11:15:24 +0100

    Update config.php

--- vozovy_park/config.php
+++ vozovy_park/config.php
@@ -2,10 +2,11 @@
 //C:\php\php.exe C:\php\php.exe -S localhost:8000
     // Funkce pro připojení k databázi
     function getDbConnection() {
-        $host = 'localhost'; // Hostitel serveru (pro lokální server nechej localhost)
-        $db = 'SpravaVozParku'; // Název databáze
-        $user = 'postgres'; // Uživatelské jméno PostgreSQL
-        $pass = '123'; // Heslo k PostgreSQL uživateli
+        $host = 'dpg-cvd87b5umphs73ea242g-a'; // Hostitel serveru (pro lokální server nechej localhost)
+        $db = 'maturitni_db'; // Název databáze
+        $user = 'maturitni_db_user'; // Uživatelské jméno PostgreSQL
+        $pass = 'qHISdm8VypBWJHPWrq1GNGFuITm7Hb37'
+        $pass = '5432'; // Heslo k PostgreSQL uživateli
     
         // Připojení k databázi
         $conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
