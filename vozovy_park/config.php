d8086eb0995f04941cde38cc71758483e1a82ef8
Ondřej Zbořil <101326894+tejmmm@users.noreply.github.com>
2025-03-19 11:48:00 +0100

    Update config.php

--- vozovy_park/config.php
+++ vozovy_park/config.php
@@ -1,20 +1,20 @@
 <?php
-//C:\php\php.exe C:\php\php.exe -S localhost:8000
-    // Funkce pro připojení k databázi
-    function getDbConnection() {
-        $host = 'dpg-cvd87b5umphs73ea242g-a'; // Hostitel serveru (pro lokální server nechej localhost)
-        $db = 'maturitni_db'; // Název databáze
-        $user = 'maturitni_db_user'; // Uživatelské jméno PostgreSQL
-        $pass = 'qHISdm8VypBWJHPWrq1GNGFuITm7Hb37'
-        $pass = '5432'; // Heslo k PostgreSQL uživateli
-    
-        // Připojení k databázi
-        $conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
-    
-        if (!$conn) {
-            die("Chyba připojení: " . pg_last_error());
-        }
-    
-        return $conn;
+// Funkce pro připojení k databázi
+function getDbConnection() {
+    $host = getenv('dpg-cvd87b5umphs73ea242g-a'); // Hostitel databáze z Environment Variables
+    $db = getenv('maturitni_db'); // Název databáze
+    $user = getenv('maturitni_db_user'); // Uživatelské jméno
+    $pass = getenv('qHISdm8VypBWJHPWrq1GNGFuITm7Hb37'); // Heslo
+    $port = getenv('5432'); // Port databáze (Render používá 5432)
+
+    // Připojení k databázi pomocí PostgreSQL
+    $conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
+
+    if (!$conn) {
+        die("Chyba připojení: " . pg_last_error());
     }
+
+    return $conn;
+}
 ?>
+
